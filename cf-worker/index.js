2aad3cced0fab8e3ccecbe9313ad5de9b857efdb
laitong <49020241+laitong@users.noreply.github.com>
2019-09-25 10:18:01 +0800

    Update index.js

--- cf-worker/index.js
+++ cf-worker/index.js
@@ -3,7 +3,7 @@
 /**
  * static files (404.html, sw.js, conf.js)
  */
-const ASSET_URL = 'https://laitong.github.io/jsproxy'
+const ASSET_URL = 'https://laitong.github.io/travel'
 
 const JS_VER = 10
 const MAX_RETRY = 1
